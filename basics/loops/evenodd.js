513abbcb77b5b80492cbd49cb7cb82c6b4137a74
amber-ahmed <aammber321@gmail.com>
2022-11-29 11:05:00 +0530

    changed evenodd.js in loops

--- basics/loops/evenodd.js
+++ basics/loops/evenodd.js
@@ -1,6 +1,11 @@
 var a = 0;
+var count =0;
+
 while(a<10){
-    console.log("odd " + (++a));
-    a = a+1;
     console.log("even " +a)
+    console.log("odd " + (++a));
+    a++;
+   count++;
 }
+console.log("even "+a)
+console.log("itereations : " +count)
