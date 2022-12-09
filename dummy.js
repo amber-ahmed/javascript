882592775f6366985b8e59912dae3958149a0e77
amber-ahmed <aammber321@gmail.com>
2022-12-09 12:38:50 +0530

    added dummy.js

---
+++ dummy.js
@@ -0,0 +1,9 @@
+var a = 0xffff
+var b = 65535
+var hexa = parseInt(b,16)
+console.log(hexa)
+if(a==b)
+    console.log("compared")
+var x =  ""
+console.log( isNaN(x))
+console.log(Number.isInteger(parseInt(x)))
